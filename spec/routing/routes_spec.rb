0e10abcf422677fbbf2e37739c89e3faf68c4cb7
Travis-Le-Duc <>
2021-01-14 16:08:28 +0100

    Add spec routes for Fighter CRUD

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -14,8 +14,8 @@ describe "routing for fighters" do
 	it "routes /fighters/:id to the show action" do
 		expect(get("/fighters/2")).to route_to(
 			controller: "fighters",
-			action: "show",
-			id: "2"
+			action:			"show",
+			id:					"2"
 		)
 	end
 
@@ -25,5 +25,29 @@ describe "routing for fighters" do
 
 	it "routes /fighters to the create action" do
 		expect(post("/fighters")).to route_to("fighters#create")
+	end
+
+	it "routes /fighters/:id/edit to the edit action" do
+		expect(get("/fighters/4/edit")).to route_to(
+			controller:	 "fighters",
+			action:			 "edit",
+			id:					 "4"
+		)
+	end
+
+	it "routes /fighters/:id to the update action" do
+		expect(put("/fighters/6")).to route_to(
+			controller:	 "fighters",
+			action:			 "update",
+			id:					 "6"
+		)
+	end
+
+	it "routes /fighters/:id to the destroy action" do
+		expect(delete("/fighters/3")).to route_to(
+			controller:	 "fighters",
+			action:			 "destroy",
+			id:					 "3"
+		)
 	end
 end
